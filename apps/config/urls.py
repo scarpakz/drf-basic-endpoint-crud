adf80f34f6386c984971282905fa908019362819
Jason Javier <jasonjjavier@gmail.com>
2021-03-22 20:13:38 +0800

    added rest framework simple jwt

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # Index
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
