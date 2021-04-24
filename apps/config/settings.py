e0c1236769fbd328579d14a9e5145be5e6a3998b
Jason Javier <jasonjjavier@gmail.com>
2021-04-24 16:46:00 +0800

    added django heroku module

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+django_heroku.settings(locals())
